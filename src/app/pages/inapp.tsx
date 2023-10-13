5032196a5adf5422458ed3b2e932ef2dd0407036
Matej Lelek <matej.lelek@deegloo.com>
2023-10-13 12:01:50 +0200

    tests for routes

---
+++ src/app/pages/inapp.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Inapp() {
+  return (
+   <div>
+    DO you work my man in app?
+   </div>
+  )
+}
---
+++ src/pages/testpage.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function TestPage() {
+  return (
+   <div>
+    DO you work my man?
+   </div>
+  )
+}
